b5881752c16d03f018ff00c0dc5e24381585ecff
fellipesnoww <fellipe2.0e@gmail.com>
2025-04-22 22:03:00 -0300

    feature: created header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import styles from './Header.module.css';
+import igniteLogo from '../assets/ignite-logo.svg';
+
 
 export function Header() {
     return (
         <header className={styles.header}>
-            <img src="" alt="" />
-            <strong>Ignite Feed</strong>
+            <img src={igniteLogo} alt="Logo do Ignite" />
         </header>
     )
 }
