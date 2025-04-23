616a7cd0074a51c351da3c680351ebff89e11034
fellipesnoww <fellipe2.0e@gmail.com>
2025-04-22 22:40:46 -0300

    feature: created profile aside

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
 import { Header } from "./components/header";
 import { Post } from "./Post"
 import './global.css';
+import styles from './App.module.css';
+import { Sidebar } from "./components/Sidebar";
 
 function App() {
 
   return (
     <div>
       <Header />
-      <Post 
-        author="Fellipe Neves"
-        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius velit, sunt ex deleniti, vero magni explicabo animi voluptatum nisi nobis consectetur aliquam itaque natus nihil numquam, repudiandae sint eos ab!"
-      />
 
-      <Post 
-        author="Joao"
-        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius velit, sunt ex deleniti, vero magni explicabo animi voluptatum nisi nobis consectetur aliquam itaque natus nihil numquam, repudiandae sint eos ab!"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+            author="Fellipe Neves"
+            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius velit, sunt ex deleniti, vero magni explicabo animi voluptatum nisi nobis consectetur aliquam itaque natus nihil numquam, repudiandae sint eos ab!"
+          />
+
+          <Post 
+            author="Joao"
+            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius velit, sunt ex deleniti, vero magni explicabo animi voluptatum nisi nobis consectetur aliquam itaque natus nihil numquam, repudiandae sint eos ab!"
+          />
+        </main>
+      </div>
     </div>
   )
 }
